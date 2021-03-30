1741e837cc996c25278a1c31a6f0dfeadd2aa8fd
jrdnc <jrdnblvn@gmail.com>
2021-03-29 20:12:22 -0500

    added comments for planning

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const categories = document.querySelectorAll('div.cats button');
 const enter = document.querySelector('div.enter button')
+let selectedCategory = null;
 
 function selectCategory(e) {
-  console.log("category!");
+  // store current button as selected category
+  selectedCategory = e.target.innerText;
+  console.log(selectedCategory);
+  //ask for usesr input based on category
+    // math & trivia - enter a specifc or randomize
+    // date - enter a date or randomize
+  // deboss button
 }
 
 function displayFact(e) {
   console.log("enter");
+  // based on current selected category, display number fact (using Numbers API)
+  // if no category selected, ask user to select a category first
 }
 
 categories.forEach(cat => cat.addEventListener('click', selectCategory)); // add event listener to each category button
