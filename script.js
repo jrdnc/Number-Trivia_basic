2519f0066d1b17a28183caa456fb4d548ba3510b
jrdnc <jrdnblvn@gmail.com>
2021-04-05 01:37:39 -0500

    added 4th category and placeholder text

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let selectedCategory = null;
 function selectCategory(e) {
   const element = document.querySelector('div.input');
   const enter = document.querySelector('div.enter');
+
   // store current button as selected category
   let temp = e.target.innerText.toLowerCase();
-  console.log(temp);
-  console.log(selectedCategory);
+
   // if same category || diff category and hidden, then toggle hidden
   if ((temp === selectedCategory) || (temp != selectedCategory && element.hasAttribute('hidden'))) {
     console.log("Hi!");
@@ -18,10 +18,9 @@ function selectCategory(e) {
   console.log(selectedCategory);
   console.log(e.target);
   console.log(element.hasAttribute('hidden'));
-  //ask for user input based on category
-  // let input;
 
-  // shift enter button based on whether user input is showing or not
+  // change height of enter button based on whether user input is showing or not
+  // need to make this not hard-coded later!!!
   if (element.hasAttribute('hidden') === true) {
     enter.style.gridRowStart = '4';
   }
@@ -29,30 +28,15 @@ function selectCategory(e) {
     enter.style.gridRowStart = '5';
   }
 
-  // create new div element to be inserted
-  // if (!document.querySelector('div.input')) {
-  //   element = document.createElement("div");
-  //   element.className += "input";
-  //   // element.className += element.classList.contains("input") ? "" : "input";
-  //   input = document.createElement("input");
-  // }
-  // else {
-  //   element = document.querySelector('div.input');
-  //   input = document/querySelector('div.input input')
-  // }
-  
-  // unhide input element
-  if (selectedCategory === "trivia") {
-
-
-
-
-    // element.style.display = "block";
-    // // place input into div
-    // element.appendChild(input);
-    // // append element to html
-    // document.querySelector('div.category').insertAdjacentElement('afterend', element);
-
+  // set placeholder text
+  if (selectedCategory === "date") {
+    element.firstElementChild.setAttribute("placeholder", "mm/dd");
+  }
+  else if (selectedCategory === "year") {
+    element.firstElementChild.setAttribute("placeholder", "yyyy");;
+  }
+  else {
+    element.firstElementChild.setAttribute("placeholder", "enter a number!");
   }
     // math & trivia - enter a specifc number or randomize
     // date - enter a date or randomize
@@ -61,6 +45,8 @@ function selectCategory(e) {
 
 function displayFact(e) {
   console.log("enter");
+  console.log(document.querySelector(".user").value);
+  
   // based on current selected category and input given from user, display number fact (using Numbers API)
   // if no category selected, ask user to select a category first
     // insert { <p class="warning">Please select a category!</p> } underneath the enter button in the HTML
