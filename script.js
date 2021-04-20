66f0ab745b0a7a44f8cdffd06518092074df0ff1
jrdnc <jrdnblvn@gmail.com>
2021-04-19 21:51:48 -0500

    corrected some of the warning functionality

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-const categories = document.querySelectorAll('div.cats button');
-const enterButton = document.querySelector('div.enter button');
+const categories = document.querySelectorAll("div.cats button");
+const enterButton = document.querySelector("div.enter button");
 let selectedCategory = null;
 
+/************************ Request User Input ************************/
 function pickCategory(e) {
-  const element = document.querySelector('div.input');
-  const enter = document.querySelector('div.enter');
+  const element = document.querySelector("div.input");
+  const enter = document.querySelector("div.enter");
 
   // store current button as selected category
   let temp = e.target.innerText.toLowerCase();
 
   // if same category || diff category and hidden, then toggle hidden
-  if ((temp === selectedCategory) || (temp != selectedCategory && element.hasAttribute('hidden'))) {
-    console.log("Hi!");
-    element.toggleAttribute('hidden');
+  if ((temp === selectedCategory) || (temp != selectedCategory && element.hasAttribute("hidden"))) {
+    element.toggleAttribute("hidden");
+    if (temp === selectedCategory) {
+      temp = null;
+    }
   }
-  selectedCategory = temp;
-  console.log(selectedCategory);
+    selectedCategory = temp;
+  
+  if (selectedCategory != null) {
+    document.querySelector(".cat-warning").setAttribute("hidden", "true");
+  }
   console.log(e.target);
-  console.log(element.hasAttribute('hidden'));
 
   // change height of enter button based on whether user input is showing or not
   // need to make this not hard-coded later!!!
-  if (element.hasAttribute('hidden') === true) {
-    enter.style.gridRowStart = '4';
+  if (element.hasAttribute("hidden") === true) {
+    enter.style.gridRowStart = "4";
   }
   else {
-    enter.style.gridRowStart = '5';
+    enter.style.gridRowStart = "5";
   }
 
   // set placeholder text
@@ -43,6 +48,7 @@ function pickCategory(e) {
   // deboss button
 }
 
+/************************ Retrieve/Display Info ************************/
 // grab fact from numbers API
 async function fetchFact(num = 111) {
   // must 'await' to allow the fetch to get us the info we need
@@ -55,23 +61,28 @@ async function fetchFact(num = 111) {
 // gets appropriate fact from Numbers API based on user input and displays on screen
 async function displayFact(e) {
   let number = document.querySelector(".user").value;
+  console.log("number is: " + number);
+  console.log("selectedCategory: " + selectedCategory);
   // if no category and/or number provided, ask user to give input first
-  if (number === undefined || selectedCategory === null) {
-    if (number === undefined) {
-      document.querySelector(".num-warning").removeAttribute('hidden');
+  if (number === "" || selectedCategory === null) {
+    if (number === "") {
+      document.querySelector(".num-warning").removeAttribute("hidden");
     }
     if (selectedCategory === null) {
-      document.querySelector(".cat-warning").removeAttribute('hidden');
+      document.querySelector(".cat-warning").removeAttribute("hidden");
     }
   }
   else {
-    document.querySelector(".ans").setAttribute('hidden');
+    console.log("hihi");
+    document.querySelector(".ans").removeAttribute("hidden");
     // if warning unhidden, rehide it
+    document.querySelector(".num-warning").setAttribute("hidden", "true");
+    document.querySelector(".cat-warning").setAttribute("hidden", "true");
     // must 'await' to allow for response from API before saving/using this info
     let fact = await fetchFact(number);
     console.log("fact: " + fact);
   }
 }
 
-categories.forEach(cat => cat.addEventListener('click', pickCategory)); // add event listener to each category button
-enterButton.addEventListener('click', displayFact);
+categories.forEach(cat => cat.addEventListener("click", pickCategory)); // add event listener to each category button
+enterButton.addEventListener("click", displayFact);
